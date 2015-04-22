362a3aa745f6fc9a57070d30543d1fb666001e0d
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-22 18:35:04 +0700

    feat(scripts): added partially-completed JS files for the app

---
+++ app/public/js/source/class.supercharged.js
@@ -0,0 +1,40 @@
+/*
+ * # The Supercharged Class
+ *
+ * This class contains methods and properties relating to the app. They're set 
+ * up in a way that allows us to quickly and easily call methods without the 
+ * need to remember lots of parameters.
+ */
+(function () {
+  'use strict';
+
+  function Supercharged( options ) {
+
+    // - Check for user-supplied options and use an empty object if none exists
+    var opts = options || {};
+
+    // - Set debug mode based on config (defaults to `false`)
+    this.debug = opts.debug || false;
+
+    // - Get the class name (for debugging)
+    var name = this.constructor.name || 'Supercharged';
+
+    // - Chooses a logger
+    this.log = this.debug ? bows(name) : function(){};
+
+    // - Create an initial log message to announce that the class was created
+    this.log('New instance created.');
+
+  }
+
+  // Using `Object.create` allows us to easily extend this object later.
+  Supercharged.prototype = Object.create({
+    constructor: {
+      value: Supercharged
+    }
+  });
+
+  // Expose the Supercharged class is available to the global scope
+  window.Supercharged = Supercharged;
+
+}).call();
---
+++ app/public/js/source/main.js
@@ -0,0 +1,19 @@
+/*
+ * # JavaScript
+ *
+ * 
+ */
+(function(  ) {
+  'use strict';
+
+  // - Create config options for the Supercharged class
+  var options = {
+      debug: true //   - Debug mode is true for development
+  };
+
+  // - Create a new instance of the Supercharged class
+  var supercharged = new Supercharged(options);
+
+  supercharged.log('Log testing.');
+
+})();

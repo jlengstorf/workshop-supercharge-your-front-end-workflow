82c63e9a8253dcde1ac8d4e93683f7380344329f
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-24 20:28:31 +0700

    undefined

--- assets/behavior.js
+++ assets/behavior.js
@@ -31,6 +31,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
               "type": "heading",
               "data": {
                 "level": 2,
+                "title": "Quick Start",
+                "slug": "quick-start"
+              },
+              "depth": 2
+            }, {
+              "type": "heading",
+              "data": {
+                "level": 2,
                 "title": "What You&#39;ll Learn",
                 "slug": "what-you39ll-learn"
               },
@@ -75,6 +83,14 @@ f.event={add:function(a,c,d,e,g){var h,i,j,k,l,m,n,o,p,q,r,s;if(!(a.nodeType===3
       "depth": 1,
       "outline": [
         {
+          "type": "heading",
+          "data": {
+            "level": 2,
+            "title": "Quick Start",
+            "slug": "quick-start"
+          },
+          "depth": 2
+        }, {
           "type": "heading",
           "data": {
             "level": 2,

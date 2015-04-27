6b67ae1a00875d942d5a23651a59293e96f129ed
jlengstorf <jason.lengstorf@copterlabs.com>
2015-04-27 15:58:35 +0100

    feat(config): added tokens for non-IG/FB users

--- app/public/js/source/config.EXAMPLE.js
+++ app/public/js/source/config.EXAMPLE.js
@@ -5,23 +5,44 @@
 (function(  ) {
   'use strict';
 
+  // window.APP_CONFIG = {
+  //   instagram: {
+
+  //     // - [Find your user ID](http://jelled.com/instagram/lookup-user-id)
+  //     userID: 'YOUR_IG_USER_ID_HERE',
+
+  //     // - [Get a token](https://instagram.com/developer/api-console/)
+  //     token: 'YOUR_IG_TOKEN_HERE'
+
+  //   },
+  //   facebook: {
+
+  //     // - [Find an album ID](http://bit.ly/1yTm0RI)
+  //     albumID: 'YOUR_FB_ALBUM_ID_HERE',
+
+  //     // - [Get a token](http://bit.ly/1DgCkqZ)
+  //     token: 'YOUR_FB_TOKEN_HERE'
+
+  //   }
+  // };
+
   window.APP_CONFIG = {
     instagram: {
 
       // - [Find your user ID](http://jelled.com/instagram/lookup-user-id)
-      userID: 'YOUR_IG_USER_ID_HERE',
+      userID: '30794906',
 
       // - [Get a token](https://instagram.com/developer/api-console/)
-      token: 'YOUR_IG_TOKEN_HERE'
+      token: '30794906.1fb234f.8faa8c1dd437479a9ca8f9dda5b202b8'
 
     },
     facebook: {
 
       // - [Find an album ID](http://bit.ly/1yTm0RI)
-      albumID: 'YOUR_FB_ALBUM_ID_HERE',
+      albumID: '756766124393291',
 
       // - [Get a token](http://bit.ly/1DgCkqZ)
-      token: 'YOUR_FB_TOKEN_HERE'
+      token: '220587498083499|VrVXAg_mqhH6cOA8OGFGR9rGyV4'
 
     }
   };
